4f37fa7549718d095356cc4d4673617d70d82bcb
SergZ <SergZ@SergZ-PC>
2018-04-13 16:26:43 +0300

    5 version

--- Git Test/Git Test/src/zs/git_test/Test.java
+++ Git Test/Git Test/src/zs/git_test/Test.java
@@ -4,8 +4,9 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("3 version");
-		Process p = new Process(1, "A1");
+		System.out.println("4 version");
+		Process p1 = new Process(1, "A1");
+		System.out.println(p1.toString());
 	}
 
 }
