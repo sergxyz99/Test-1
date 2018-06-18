daf37c464ea525eb3d9463fea759497d78ea2426
SergZ <SergZ@SergZ-PC>
2018-06-18 20:41:06 +0300

    8 version for task#1

--- Git Test/Git Test/src/zs/git_test/Test.java
+++ Git Test/Git Test/src/zs/git_test/Test.java
@@ -3,7 +3,7 @@ package zs.git_test;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("7 version");
+		System.out.println("8 version");
 		Process p1 = new Process(1, "A1");
 		System.out.println(p1.toString() + " from task#1 branch");
 	}
