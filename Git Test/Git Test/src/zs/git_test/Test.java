89a00cc68365b8e80428244a1af9c3a4e25bb9aa
SergZ <SergZ@SergZ-PC>
2018-06-18 20:15:46 +0300

    7 version task#1

--- Git Test/Git Test/src/zs/git_test/Test.java
+++ Git Test/Git Test/src/zs/git_test/Test.java
@@ -4,9 +4,9 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("6 version");
+		System.out.println("7 version");
 		Process p1 = new Process(1, "A1");
-		System.out.println(p1.toString());
+		System.out.println(p1.toString() + " from task#1 branch");
 	}
 
 }
